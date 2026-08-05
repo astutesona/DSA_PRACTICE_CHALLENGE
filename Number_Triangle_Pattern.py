935cb8fc767fe486152238c68d153e63ef2c31ad
Sonali Kumari <astutesona@gmail.com>
2026-08-06 00:39:12 +0530

    Create Number_Triangle_Pattern.py

---
+++ Number_Triangle_Pattern.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pattern4(self, n):
+        for i in range(1, n + 1):
+            for j in range(i):
+                print(i, end="")
+            print()
+
+m = Solution()
+n = 4
+m.pattern4(n)
